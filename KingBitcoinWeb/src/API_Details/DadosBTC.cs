a7b3958a9d343e3a6a1d42f73d9e7172598a846e
Fabricio <fabricio.beretta@acad.pucrs.br>
2020-04-05 20:56:54 -0300

    Novas Classe -- Com API

---
+++ KingBitcoinWeb/src/API_Details/DadosBTC.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+/*"high":"35850.00000000",
+"low":"35351.00000000",
+"vol":"48.52519108",
+"last":"35646.66677000",
+"buy":"35390.00406000",
+"sell":"35626.65022000",
+"open":"35568.38182000",
+"date":1586125550}}
+
+ public int Id { get; set; }
+     */
+namespace RequisicoesWeb
+{
+    class DadosBTC
+    {
+        public String hing { get; set; }
+        public String low { get; set; }
+        public String vol { get; set; }
+        public String last { get; set; }
+        public String buy { get; set; }
+        public String sell { get; set; }
+        public String open { get; set; }
+        public String date { get; set; }
+        public String ticker { get; set;}
+    }
+}
